b9ae264cd9e6b13f0ad726c05325d15ab91751d3
WUTONK <wjt474696120@gmail.com>
2024-02-17 21:01:23 +0800

    退回函数重新整合，已经可以正常检测子集名，但map输出有问题

--- lib/get_dir.dart
+++ lib/get_dir.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:ffi';
 import 'dart:io';
 
 void main() {
@@ -25,14 +24,15 @@ void getAcPath() {
 
 Map<String, dynamic> _vdfToMap(String vdfContent) {
   var lines = LineSplitter.split(vdfContent).toList(); // 按行分割VDF内容
-  var vdfData = <String, dynamic>{}; // 存储JSON数据的Map
+  var vdfData = <String, dynamic>{}; // 存储VDF数据的Map
   var stack = <Map<String, dynamic>>[]; // 用于模拟堆栈的列表，存储当前正在处理的层级的Map
-  var mapName = String;
-  
+  var mapName = '';
+
   stack.add(vdfData); // 将根Map添加到堆栈中
+  print(stack);
 
   for (var line in lines) {
-    
+    print(mapName);
     line = line.trim(); // 去除首尾空格
     if (line.isEmpty){
       continue; // 跳过空行
@@ -43,37 +43,33 @@ Map<String, dynamic> _vdfToMap(String vdfContent) {
       var parts = line.split('\t'); // 使用制表符分割键值对
       var key = parts[0].replaceAll('"', ''); // 获取键并移除引号
 
-      //检测是否符合子集名特性
-      if(!_IsMapName(parts,key)){
-        var value = parts[2].replaceAll('"', '');
-        stack.last[key] = value; // 在堆栈顶部(last)的Map中添加键值对
-      } else{
-        mapName = key;
+      //map名称后没有字符，所以会异常，利用此特性获取子集名
+      try {
+        var value = parts[2].replaceAll('"', ''); 
+      } catch (e) {
+        print('获取值失败，其可能是子集名');
+        mapName = key.toString();
+        continue;
       }
 
-      
+      var value = parts[2].replaceAll('"', '');
+      stack.last[key] = value; // 在堆栈顶部(last)的Map中添加键值对
+
     } else if (line == '{') { //如果如果行以'{'开始
+      if(mapName != ''){
       print(stack);
       var newMap = <String, dynamic>{}; // 创建一个新的Map表示新的层级
       stack.last[mapName] = newMap; // 将新的Map添加到当前层级的Map中，命名为之前获取的子集名
-      stack.add(newMap); // 将新的Map推入堆栈
       print(stack);
+      // stack.add(newMap); // 将新的Map推入堆栈
+      // print(stack);
+      }else{
+        print("未能获取到子集名mapName，无法添加集合");
+      }
     } else if (line == '}') { 
       stack.removeLast(); // 当前层级处理完毕，从堆栈中移除
     }
   }
 
   return vdfData; // 返回转换后的map数据
-}
-
-//map名称后没有字符，所以会异常，利用此特性获取子集名
-bool _IsMapName(parts,key){
-  try {
-        var value = parts[2].replaceAll('"', ''); 
-      } catch (e) {
-        print('获取值失败，其可能是子集名');
-        var mapName = key.toString();
-        return true;
-      }
-  return false;
 }
