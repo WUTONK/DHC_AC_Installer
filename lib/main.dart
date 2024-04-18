7103806c99d1771bfe6d0a9d02453e446facd1e2
WUTONK <wjt474696120@gmail.com>
2024-04-19 03:03:01 +0800

    添加了i10n国际化插件，使用教程链接在pubsepc.yaml文件中，添加了中，英，日翻译文件

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'ui_test/drawer_test/darawer_test.dart' as test_ui;
+import 'package:flutter_localizations/flutter_localizations.dart'; //国际化
+import 'generated/l10n.dart'; //同上
+void main() {
+  runApp(test_ui.MyApp());
+}
 
-// void main() {
-//   runApp(test_ui.MyApp());
-// }
