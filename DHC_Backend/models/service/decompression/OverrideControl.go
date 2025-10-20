bdb14b162aadbbb11495b5047419cc180925a705
WUTONK <wjt474696120@gmail.com>
2025-10-20 09:25:02 +0800

    Update OverrideControl.go

--- DHC_Backend/models/service/decompression/OverrideControl.go
+++ DHC_Backend/models/service/decompression/OverrideControl.go
@@ -7,7 +7,7 @@ import (
 )
 
 // OverrideControlConfig 覆盖控制配置结构体
-type OverrideControlConfig struct {
+type DhcFileTagConfig struct {
 	ModType       string        `json:"ModType"`
 	DefaultAction DefaultAction `json:"defaultAction"`
 	Rules         []Rule        `json:"rules"`
@@ -33,7 +33,7 @@ func OverrideControl(srcDir string, dstDir string, dftPath string) error {
 	// --- 现在编写部分 ---
 
 	// 解码文件
-	config, err := decodeOverrideControlConfig(dftPath)
+	config, err := decodeDhcFileTagConfig(dftPath)
 	if err != nil {
 		return fmt.Errorf("解码覆盖控制配置文件失败: %v", err)
 	}
@@ -51,9 +51,15 @@ func OverrideControl(srcDir string, dstDir string, dftPath string) error {
 		fmt.Printf("规则 %d: 模式=%s, 操作=%s, 备份=%t\n", i+1, rule.Pattern, rule.Action, rule.Backup)
 	}
 
-	// --- 以后完成 ---
 	// 辨别是否存在任何需备份文件（包含默认和指定路径操作）如果是那么创建备份文件夹
+	// 首先辨别默认操作是否有需要备份
+	if defaultAction.Backup {
 
+	}
+
+	// 然后辨别[]rule中是否有要备份的项目
+
+	// --- 以后完成 ---
 	// 遍历每一个文件并进行操作
 
 	// 检测是否符合路径规则 如果是就按照操作执行 否则按照指定默认属性执行
@@ -64,7 +70,7 @@ func OverrideControl(srcDir string, dstDir string, dftPath string) error {
 }
 
 // decodeOverrideControlConfig 解码覆盖控制配置文件
-func decodeOverrideControlConfig(dftPath string) (*OverrideControlConfig, error) {
+func decodeDhcFileTagConfig(dftPath string) (*DhcFileTagConfig, error) {
 	// 检查文件是否存在
 	if _, err := os.Stat(dftPath); os.IsNotExist(err) {
 		return nil, fmt.Errorf("配置文件不存在: %s", dftPath)
@@ -77,10 +83,14 @@ func decodeOverrideControlConfig(dftPath string) (*OverrideControlConfig, error)
 	}
 
 	// 解码 JSON
-	var config OverrideControlConfig
+	var config DhcFileTagConfig
 	if err := json.Unmarshal(fileData, &config); err != nil {
 		return nil, fmt.Errorf("解析 JSON 配置失败: %v", err)
 	}
 
 	return &config, nil
+}
+
+func createBackupFile() {
+
 }
